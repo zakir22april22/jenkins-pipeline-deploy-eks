4bfb56ad407845d004864512427fffa4856574e2
zakir <zk3644589@gmail.com>
2023-06-18 11:23:43 +0530

    jenkins-pipeline

---
+++ jenkins-pipeline-deploy-eks/terraform/eks-cluster.tf
@@ -0,0 +1,25 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "myapp-eks-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access  = true
+  vpc_id = 
+  subnet_ids = 
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+  eks_managed_node_groups = {
+    dev = {
+        min_size = 1
+        max_size = 3
+        desired_size = 2
+
+        instance_type = ["t2.small"]
+    }
+  }
+}
---
+++ jenkins-pipeline-deploy-eks/terraform/vpc.tf
@@ -0,0 +1,31 @@
+data "aws_availability_zones" "azs" {}
+module "myapp-vpc" {
+    source = "terraform-aws-modules/eks/aws"
+    version = "3.19.0"
+    name = "myapp-vpc"
+    cidr = "10.0.0.0/16"
+    private_subnet = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+    public_subnet = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+    azs = data.aws_availability_zones.azs.names
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostname = true
+
+    tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    }
+
+    public_subnet_tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+         "kubernetes.io/role/internal-elb" = "1"
+
+    }
+    private_subnet_tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+         "kubernetes.io/role/internal-elb" = "1"
+
+    }
+
+  
+}
